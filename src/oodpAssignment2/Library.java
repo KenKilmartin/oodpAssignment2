abb7228db0407934ced930b13c99ec1fadbc715c
CathPhoenix <catharine_s@hotmail.com>
2018-03-12 21:54:35 +0000

    GUI still not running, but with print statements can see that information is getting correctly

--- src/oodpAssignment2/Library.java
+++ src/oodpAssignment2/Library.java
@@ -16,12 +16,15 @@ public class Library extends JFrame{
 		makeGUI();
 	}
 	private void makeGUI() {
+		c.setLayout(new BorderLayout());
 		JPanel centerPanel = new JPanel();
 		JLabel label1 = new JLabel(bookFactory.getBook("Harry Potter").getAuthor());
 		JLabel label2 = new JLabel(cdFactory.getCd("Californication").getName());
-		centerPanel.add(label1);
-		centerPanel.add(label2);
+		centerPanel.add(label1, BorderLayout.NORTH);
+		centerPanel.add(label2, BorderLayout.SOUTH);
 		c.add(centerPanel);
+		System.out.println("Author of harry potter: "+bookFactory.getBook("Harry Potter").getAuthor());
+		System.out.println("cd name: "+cdFactory.getCd("Californication").getName());
 		
 		c.setBounds(600, 600, 600, 600);
 		c.setVisible(true);
