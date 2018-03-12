cf3d75637fb071bd26ffdfaaaf1166f58c8f4ee6
KenKilmartin <kenkilmartin@gmail.com>
2018-03-12 11:18:52 +0000

    just putting in adapter class

---
+++ src/oodpAssignment2/ComicAdapter.java
@@ -0,0 +1,7 @@
+package oodpAssignment2;
+
+// was thinking this could be the adapter class ? 
+
+public class ComicAdapter {
+
+}
--- src/oodpAssignment2/Library.java
+++ src/oodpAssignment2/Library.java
@@ -2,4 +2,6 @@ package oodpAssignment2;
 
 public abstract class Library {
 
+	
+	public static void main (String[]args) {}
 }
